2f17c554cde6aa0085375587b175e0fe50dd92af
AchrafAtt <G134219290@taalim.ma>
2025-04-04 15:41:14 +0000

    test bad pr

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,7 +22,7 @@ class User extends Authenticatable
         'email',
         'password',
     ];
-
+//!just a bad pull request
     /**
      * The attributes that should be hidden for serialization.
      *
